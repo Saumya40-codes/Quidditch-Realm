2f90053fc94d4997dadf28eb1912a6bd74ecbde1
Saumya <saumyabshah90@gmail.com>
2023-07-14 22:46:05 +0530

    nav added to dashboard

--- src/components/Admin/AdminDashboard.js
+++ src/components/Admin/AdminDashboard.js
@@ -1,99 +1,253 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { Button, Avatar } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { setLogout } from '../../state';
-import { useNavigate } from 'react-router-dom';
-import { Badge } from '@mui/material';
-import badge from '../../assets/badge.png';
-import { styled } from '@mui/material/styles';
-import { Tooltip } from '@mui/material';
+import * as React from 'react';
+import { styled, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MatchIcon from '@mui/icons-material/SportsSoccer';
+import RosterIcon from '@mui/icons-material/Group';
+import TicketIcon from '@mui/icons-material/EventSeat';
 import { Link } from 'react-router-dom';
-import Stepper from './AddEvent/Stepper';
+import { useSelector } from 'react-redux';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const StyledBadge = styled(Badge)(({ theme }) => ({
-  '& .MuiBadge-badge': {
-    backgroundColor: '#44b700',
-    color: '#44b700',
-    width:"20px",
-    height:"20px",
-    borderRadius:"50%",
-    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-    '&::after': {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      width: '100%',
-      height: '100%',
-      borderRadius: '50%',
-      animation: 'ripple 1.2s infinite ease-in-out',
-      border: '1px solid currentColor',
-      content: '""',
-    },
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
   },
-  '@keyframes ripple': {
-    '0%': {
-      transform: 'scale(.8)',
-      opacity: 1,
-    },
-    '100%': {
-      transform: 'scale(2.4)',
-      opacity: 0,
-    },
-  },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'flex-end',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
 }));
 
-const AdminDashboard = () => {
-  const user = useSelector((state) => state.user);
-  const name = user ? user.username : '';
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
 
-  const logout = () => {
-    dispatch(setLogout());
-    navigate('/admin/login');
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+      ...openedMixin(theme),
+      '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+      ...closedMixin(theme),
+      '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+  }),
+);
+
+export default function AdminDashboard() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const user = useSelector((state) => state.user);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
   };
 
   return (
-    <div>
-      Hello {name}
-      <div style={{ display: 'flex', justifyContent: 'right', marginRight:"20px"}}>
-        <StyledBadge 
-          overlap="circular"
-          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-          variant="dot"
-          borderRadius="50%"
-        >
-        <Tooltip title="Avatar" arrow>
-          <Avatar alt="Wizard" src={badge} style={{border:"2px solid blue",borderRadius:"50%", background:"azure",width:"70px",height:"70px"}} />
-        </Tooltip>
-        </StyledBadge>
-      </div>
-      <Button type='contained'>
-        <Link to="/add/event" style={{ textDecoration: "none" }}>
-      Event Enchanter
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: 'none' }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Quidditch Team Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+        <ListItem disablePadding sx={{ display: 'block', marginBottom: '10px' }}>
+        <ListItemButton
+      sx={{
+        minHeight: 48,
+        display: open ? 'flex' : 'none',
+        justifyContent: open ? 'initial' : 'right',
+        px: 2.5,
+      }}
+    >
+    <Typography variant="subtitle1" component="p" sx={{ textAlign: 'center', fontWeight: 'bold', top:"0px", left:"0px" }}>
+      Welcome, {user.username}
+    </Typography>
+    </ListItemButton>
+  </ListItem>
+  <ListItem disablePadding sx={{ display: 'block' }}>
+    <ListItemButton
+      sx={{
+        minHeight: 48,
+        justifyContent: open ? 'initial' : 'center',
+        px: 2.5,
+      }}
+    >
+      <ListItemIcon
+        sx={{
+          minWidth: 0,
+          mr: open ? 3 : 'auto',
+          justifyContent: 'center',
+        }}
+      >
+        <MatchIcon />
+      </ListItemIcon>
+      <Link to="/schedule" style={{ textDecoration: 'none' }}>
+        <ListItemText primary="Upcoming Matches" sx={{ opacity: open ? 1 : 0 }} />
       </Link>
-      </Button>
-      <Button type='contained'>
-        <Link to="/admin/add/team" style={{ textDecoration: "none" }}>
-      Team Enchanter
-      </Link>
-      </Button>
-      <Button type='contained'>
-        <Link to="/schedule" style={{ textDecoration: "none" }}>
-      Schedule Enchanter
-      </Link>
-      </Button>
-      <Button type='contained'>
-        <Link to="/past/events" style={{ textDecoration: "none" }}>
-      Past Events Enchanter
-      </Link>
-      </Button>
-      <div style={{ position: 'absolute', bottom: 0, right: 0 }}>
-        <Button onClick={logout}>Logout</Button>
-      </div>
-    </div>
-  );
-};
+    </ListItemButton>
+  </ListItem>
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <MatchIcon />
+              </ListItemIcon>
+              <Link to="/past/events" style={{ textDecoration: 'none'}}>
+              <ListItemText primary="Past Matches" sx={{ opacity: open ? 1 : 0 }} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <RosterIcon />
+              </ListItemIcon>
+              <Link to="/admin/add/team" style={{ textDecoration: 'none'}}>
+              <ListItemText primary="Team Rosters" sx={{ opacity: open ? 1 : 0 }} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                px: 2.5,
+              }}
+            >
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : 'auto',
+                  justifyContent: 'center',
+                }}
+              >
+                <TicketIcon />
+              </ListItemIcon>
+              <Link to="/add/event" style={{ textDecoration: 'none'}}>
+              <ListItemText primary="Add Events" sx={{ opacity: open ? 1 : 0 }} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <Typography>
 
-export default AdminDashboard;
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
