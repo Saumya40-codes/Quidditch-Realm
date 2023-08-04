8597fbec13b2e8a76ad428ae91c3345e9d1ba999
Saumya <saumyabshah90@gmail.com>
2023-08-04 07:50:38 +0530

    bg change

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,13 +3,12 @@ import { Button, Box, Typography } from '@mui/material';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagic, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import landing from '../assets/landing.jpg';
 import { Tooltip } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import '../App.css';
 
 const ParchmentBackground = styled('div')({
-  backgroundImage: `url(${landing})`,
+  backgroundImage: 'url(https://i.pinimg.com/originals/8f/90/58/8f90581e86c50845d120251e7b244a8a.jpg)',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   position: 'relative',
@@ -29,7 +28,7 @@ const HeaderText = styled('span')({
   fontFamily: "'Dancing Script', cursive",
   fontSize: '2.5rem',
   fontWeight: 'bold',
-  color: 'white',
+  color: '#060b26',
   fontSize: "70px",
 });
 
@@ -40,20 +39,22 @@ const LandingPage = () => {
       <Typography variant="h2" component="h1" gutterBottom>
         <HeaderText>Quidditch Realm</HeaderText>
       </Typography>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh', gap: '100px', mt: '100px' }}>
+      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh', gap: '100px', mt: '100px' }}>
         <Box sx={{ position: "relative", left: -12 }}>
           <Link to="/login" style={{ textDecoration: "none" }}>
             <Button variant="contained" sx={{
-              width: "200px",
-              height: "50px",
+              width: "280px",
+              height: "58px",
               borderRadius: "30px",
-              background: "linear-gradient(45deg, #FFA500, #0000FF, #8A2BE2)",
+              background: "transparent",
+              boxShadow:'17px 17px 21px 17px rgba(0,0,0,0.52)',
               color: "white",
               fontWeight: "bold",
               fontSize: "20px",
               transition: "box-shadow 0.3s ease",
               "&:hover": {
-                boxShadow: "6px 6px 9px 6px rgba(233, 3, 3, 0.5)",
+                background:"transparent",
+                boxShadow: "5px 5px 9px 6px rgba(233, 0, 0, 0.55)",
               },
             }}>
               <FontAwesomeIcon icon={faMagic} style={{ marginRight: "8px" }} />
@@ -67,25 +68,27 @@ const LandingPage = () => {
             top: "50%",
             left: "calc(100% + 15px)",
             transform: "translateY(-50%)",
-            width: "1px",
+            width: "3px",
             height: "40px",
             background: "white"
           }} />
         </Box>
-        <Box sx={{ position: "relative", left: -20 }}>
+        <Box sx={{ position: "relative", left: 100 }}>
           <Link to="/admin/login" style={{ textDecoration: "none" }}>
             <Button variant="contained" sx={{
-              width: "200px",
-              height: "50px",
+              width: "280px",
+              height: "58px",
               borderRadius: "30px",
               marginLeft:"15px",
-              background: "linear-gradient(45deg, #FFA500, #0000FF, #8A2BE2)",
+              background: "transparent",
+              boxShadow:'18px 17px 21px 21px rgba(0,0,0,0.65)',
               color: "white",
               fontWeight: "bold",
               fontSize: "20px",
               transition: "box-shadow 0.3s ease",
               "&:hover": {
-                boxShadow: "6px 6px 9px 6px rgba(233, 3, 3, 0.5)",
+                background:"transparent",
+                boxShadow: "5px 5px 9px 6px rgba(233, 0, 0, 0.55)",
               },
             }}>
               <FontAwesomeIcon icon={faUser} style={{ marginRight: "8px" }} />
@@ -99,7 +102,7 @@ const LandingPage = () => {
             top: "50%",
             left: "calc(-30px - 1px)",
             transform: "translateY(-50%)",
-            width: "1px",
+            width: "3px",
             height: "40px",
             background: "white"
           }} />
